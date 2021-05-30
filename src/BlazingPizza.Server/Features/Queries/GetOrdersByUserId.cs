7b7d336f53a1412092a9a59646c1b069e1d28e56
Brian Dunzweiler <briandunz@yahoo.com>
2021-05-30 15:45:26 -0400

    stubbed out query.getordersbyuserid

---
+++ src/BlazingPizza.Server/Features/Queries/GetOrdersByUserId.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+
+namespace BlazingPizza.Server.Features.Queries
+{
+    public class GetOrdersByUserId
+    {
+        public class Query: IRequest<Result>
+        {
+            public string UserId { get; set; }
+        }
+
+        // server side validation
+        public class QueryValidator: AbstractValidator<Query>
+        {
+            public QueryValidator()
+            {
+                RuleFor(x => x.UserId).NotEmpty();
+            }
+        }
+
+        public class Result
+        {
+            // fill in order details
+        }
+
+        public class GetOrdersByUserIdHandler : IRequestHandler<Query, Result>
+        {
+            public GetOrdersByUserIdHandler()
+            {
+                // retrieve the orders
+            }
+
+            public async Task<Result> Handle(Query request, CancellationToken cancellationToken)
+            {
+                await Task.Delay(1); // stubbed out
+                return new Result();
+            }
+        }
+    }
+}
