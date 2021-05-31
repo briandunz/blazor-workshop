dc899aae35d5c772573acc12ae58afda949c7965
Brian Dunzweiler <briandunz@yahoo.com>
2021-05-31 12:02:47 -0400

    added topping test

--- src/BlazingPizza.Shared.Test/OrderTest.cs
+++ src/BlazingPizza.Shared.Test/OrderTest.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using BlazingPizza;
 using Xunit;
 
 namespace BlazingPizza.Shared.Test
---
+++ src/BlazingPizza.Shared.Test/ToppingTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace BlazingPizza.Shared.Test
+{
+    public class ToppingTest
+    {
+        [Fact]
+        public void PriceFormatterReturnsFormattedString()
+        {
+            var subjectPrice = 1.00M;
+            var subject = new Topping { Price = subjectPrice };
+
+            Assert.Equal(subjectPrice.ToString("0.00"), subject.GetFormattedPrice());
+        }
+    }
+}
