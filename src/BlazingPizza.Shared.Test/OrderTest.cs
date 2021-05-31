d4e041bf945d82df6a08b0a2aa801f8c62015d5f
Brian Dunzweiler <briandunz@yahoo.com>
2021-05-31 11:54:13 -0400

    adding in unit tests for domain

---
+++ src/BlazingPizza.Shared.Test/OrderTest.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using BlazingPizza;
+using Xunit;
+
+namespace BlazingPizza.Shared.Test
+{
+    public class OrderTest
+    {
+        [Fact]
+        public void OrderTotalsPriceForSinglePizzaNoToppings()
+        {
+            var pizzaSpecial = new PizzaSpecial
+            {
+                BasePrice = 11.50M
+            };
+            var pizza = new Pizza
+            {
+                Special = pizzaSpecial,
+                Size = 12,
+                Toppings = new List<PizzaTopping>()
+            };
+            var order = new Order
+            {
+                Pizzas = new List<Pizza> { pizza }
+            };
+
+            Assert.Equal(pizzaSpecial.BasePrice, order.GetTotalPrice());
+        }
+
+        [Fact]
+        public void OrderTotalsPriceForMultiplePizzaNoToppings()
+        {
+            var pizzaSpecial1 = new PizzaSpecial
+            {
+                BasePrice = 11.50M
+            };
+            var pizzaSpecial2 = new PizzaSpecial
+            {
+                BasePrice = 12.50M
+            };
+            var pizza1 = new Pizza
+            {
+                Special = pizzaSpecial1,
+                Size = 12,
+                Toppings = new List<PizzaTopping>()
+            };
+            var pizza2 = new Pizza
+            {
+                Special = pizzaSpecial2,
+                Size = 12,
+                Toppings = new List<PizzaTopping>()
+            };
+            var order = new Order
+            {
+                Pizzas = new List<Pizza> { pizza1, pizza2 }
+            };
+
+            Assert.Equal(pizzaSpecial1.BasePrice + pizzaSpecial2.BasePrice, order.GetTotalPrice());
+        }
+    }
+}
